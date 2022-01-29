8f78f6e08c380005f1ba9fb4a2865a13524ae6e0
nihal <nihalsuri1619@gmail.com>
2022-01-29 19:17:48 +0100

    update MATLAB and simulink model

--- Simulations/MATLAB/TransferFunctionAndSS.m
+++ Simulations/MATLAB/TransferFunctionAndSS.m
@@ -2,13 +2,13 @@ clear all;
 close all;
 
 % PARAMETERS
-m = 0.11;   % mass of the ball [kg]      ADJUST TO REAL BALL MASS 
-R = 0.015;   % radius of the ball [m]     ADJUST TO REAL BALL RADIUS
-d = 0.03;   % lever arm offset [m]       ADJUST TO REAL LEVER ARM OFFSET
+m = 0.11;   % mass of the  object[kg]      
+R = 0.015;   % radius of the object[m]     
+d = 0.03;   % lever arm offset [m]      
 g = 9.8;    % gravitational acceleration
-L = 1.0;    % length of the beam         ADJUST TO REAL BEAM LENGTH
-J = 9.99e-6;    % ball's moment of inertia       ADJUST TO REAL VALUE
-r = 0;      % ball position coordinate
+L = 1.0;    % length of the beam         
+J = 9.99e-6;    % object's moment of inertia       
+r = 0;      % object position coordinate
 alpha = 0;  % beam angle (radians)
 theta = 0;  % servo angle (radians)
 
@@ -23,7 +23,7 @@ A = [0 1 0 0; 0 0 H 0; 0 0 0 1; 0 0 0 0]
 B = [0 0 0 1]';
 C = [1 0 0 0];
 D = [0];
-ball_ss = ss(A,B,C,D)
+object_ss = ss(A,B,C,D)
 
 
 
